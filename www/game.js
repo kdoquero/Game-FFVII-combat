44fd889e17fa1d5cb60383d7973027a36d3b90c7
kdoquero <kevindoquero@gmail.com>
2018-01-09 22:30:00 +0100

    ajout button et function block

--- www/game.js
+++ www/game.js
@@ -1,17 +1,19 @@
 
 
 
-let Cloud = new Perso('Cloud',100,10,7,2);
+let Cloud = new Perso('Cloud',100,10,7,4,2);
 
-let Sephiroth = new Perso('Sephiroth',150,5,9,1);
+let Sephiroth = new Perso('Sephiroth',200,5,11,2,1);
 
-function isDead() {
-    let pv = Sephiroth.pv;
-    if (pv === 0) {
-        Sephiroth.isDead = true;
+function endGame(pvSephiroth,pvCloud) {
+    if (pvSephiroth == 0) {
+        console.log("Sephiroth died");
 
-        return 'Sephiroth is Dead !!!';
         
+    }
+
+    if (pvCloud == 0) {
+        console.log("Cloud died");
     }
     
 };
@@ -19,27 +21,48 @@ function isDead() {
 let form = document.querySelector('form');
 let attack = document.querySelector('#attack');
 let skill = document.querySelector('#skill');
+let block = document.querySelector('#block');
 let item = document.querySelector('#item');
 
 attack.addEventListener('click' , function(event){
-    Sephiroth.pv = Sephiroth.pv - Cloud.str;
-    console.log(`Sephiroth perd ${Cloud.str}, il lui reste ${Sephiroth.pv} pv !!!`);
-    isDead();
+    Sephiroth.pv = Sephiroth.pv - Cloud.attack1();
+    Cloud.pv = Cloud.pv - Sephiroth.attack2();
+    console.log(`Sephiroth perd ${Cloud.attack1()}, il lui reste ${Sephiroth.pv} pv !!!`);
+    console.log(`Cloud perd ${Sephiroth.attack2()}, il lui reste ${Cloud.pv} pv !!!`);
+    let pvSephiroth = Sephiroth.pv;
+    let pvCloud = Cloud.pv;
+    endGame(pvSephiroth,pvCloud);
 });
 
-/* array.forEach(element => {
-    
-}); */
 
 skill.addEventListener('click' , function(event){
-    console.log(Cloud.skill());
+    Sephiroth.pv = Sephiroth.pv - Cloud.skill();
+    Cloud.pv = Cloud.pv - Sephiroth.attack2();
+    console.log('Climhazard !!!');
+    console.log(`Sephiroth perd ${Cloud.skill()}, il lui reste ${Sephiroth.pv} pv !!!`);
+    console.log(`Cloud perd ${Sephiroth.attack2()}, il lui reste ${Cloud.pv} pv !!!`);
+    let pvSephiroth = Sephiroth.pv;
+    let pvCloud = Cloud.pv;
+    endGame(pvSephiroth,pvCloud);
+    
     
 });
 
-item.addEventListener('click' , function(event){
+block.addEventListener('click' , function(event){
+    console.log('Sephiroth attaque pendant que Cloud bloque !!!');
+    Cloud.pv = Cloud.pv - Sephiroth.attack2() + Cloud.block();
+    let calculDef = Sephiroth.attack2() - Cloud.block();
+     console.log(`Cloud perd ${calculDef}, il lui reste ${Cloud.pv} pv !!!`);
+     let pvSephiroth = Sephiroth.pv;
+     let pvCloud = Cloud.pv;
+     endGame(pvSephiroth,pvCloud);
+    
+});
+
+/* item.addEventListener('click' , function(event){
     console.log(Cloud.useItem());
     
 });
-
+ */
 
 
--- www/perso.js
+++ www/perso.js
@@ -1,20 +1,37 @@
 class Perso {
-    constructor(name,pv,cp,str,item,isDead=false) {
+    constructor(name,pv,cp,str,def,item,isDead=false) {
         this.name = name;
         this.pv = pv;
         this.cp = cp;
         this.str = str;
+        this.def = def;
         this.item = item;
         this.isDead = isDead;
     }
 
-    attack() {
-    
+    attack1() {
+        let str = this.str + Math.floor((Math.random() * 9) + 0);
+        return str;
+    }
+
+    attack2() {
+        let str = this.str + Math.floor((Math.random() * 3) + 0);
+        return str;
     }
 
     skill() {
-        console.log('Climhazard');
+        let str = this.str *3 + Math.floor((Math.random() * 10) + 1);
 
+        return str;
+
+    }
+
+    block() {
+        let def = this.def + Math.floor((Math.random() * 4) + 0);
+        let pv = this.pv + 2 + Math.floor((Math.random() * 12) + 0);
+
+        return def;
+        return pv;
     }
 
     useItem() {
